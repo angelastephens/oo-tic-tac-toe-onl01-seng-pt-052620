d51d95633aaadafaa24fed5c0a5180219923ba99
Angela Stephens <angelastephens317@gmail.com>
2020-06-29 23:41:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,12 +79,7 @@ class TicTacToe
     end
   end 
   
-  def won?
-    win_combinations.find do |win_combo|
-      position_1_index= win_combo[0]
-      position_2_index= win_combo[1]
-      position_3_index= win_combo[2]
-  end
+  /
 end
 
 
