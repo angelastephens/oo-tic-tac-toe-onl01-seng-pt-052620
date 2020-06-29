df81486d968f2d0cc980b3047a60d99ad08e0487
Angela Stephens <angelastephens317@gmail.com>
2020-06-29 23:14:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,11 +70,11 @@ class TicTacToe
       index = -1
         until valid_move?(index)
          puts "Please make a move"
-          input = gets.strip
-          index = input_to_index(input)
-        if valid_move?(index)
-      move(index,current_player)
-        display_board
+         input = gets.strip
+         index = input_to_index(input)
+         if valid_move?(index)
+          move(index,current_player)
+           display_board
        end
     end
   end 
