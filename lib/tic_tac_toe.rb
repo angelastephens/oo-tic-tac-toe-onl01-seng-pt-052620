72cba775ad7474b92cb55efdf60df677597c6b42
Angela Stephens <angelastephens317@gmail.com>
2020-06-30 00:04:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,8 +86,8 @@ class TicTacToe
       position_2_token = board[position_2_index]
       position_3_token = board[position_3_index]
       position_1_token == position_2_token &&
-      position_2_token == position_3_token 
-      # position_taken?(postion_1_index)
+      position_2_token == position_3_token &&
+      position_taken?(postion_1_index)
     end
   end
   
